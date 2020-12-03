b0b1dcdad959c75abd20cf32c02d62a3512ea7e0
RSGDesign <75441866+RSGDesign@users.noreply.github.com>
2020-12-04 00:38:44 +0200

    Add files via upload

---
+++ lite_settings.php
@@ -0,0 +1,7 @@
+<?php
+
+$email_to = "sebastianfierforjat@gmail.com"; // your email address
+$email_subject = "mesaj: Sebastian fier forjat srl"; // email subject line
+$thankyou = "thankyou.htm"; // thank you page
+
+?>
